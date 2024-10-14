671b94355cb57ad01a4a6333bd97584d2a2dc6d5
Albarra Naufala <albarraerdanto@gmail.com>
2024-10-14 21:34:19 +0700

    add swipe guide animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,42 @@ module.exports = {
         "cedarville-cursive": ['"Cedarville Cursive"', "cursive"],
         "abril-fatface": ['"Abril Fatface"', "serif"],
       },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        "swipe-left": {
+          "0%": {
+            transform: "scale(1.2) translateX(0)",
+            opacity: "0",
+          },
+          "10%": {
+            transform: "scale(1.2) translateX(0)",
+            opacity: "0",
+          },
+          "20%": {
+            transform: "scale(1) translateX(0)",
+            opacity: "1",
+          },
+          "80%": {
+            transform: "scale(1) translateX(-200px)",
+            opacity: "1",
+          },
+          "90%": {
+            transform: "scale(1) translateX(-200px)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1.2) translateX(0)",
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 3s ease-in-out infinite",
+        "swipe-left": "swipe-left 3s ease-in-out infinite",
+      },
     },
   },
   plugins: [],
